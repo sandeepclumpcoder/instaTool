b53dde0a3ec20e2fe5442cfebabe3c8de516dd44
sandeep vishwakarma <sandeepclumpcoder@gmail.com>
2022-04-07 06:26:10 -0700

    add code in db

--- db.js
+++ db.js
@@ -1,14 +1,51 @@
 const mongoose = require('mongoose');
 
-const mongoURI =  "mongodb://127.0.0.1:27017/instaTool"
+const mongoURI = "mongodb://127.0.0.1:27017/instaTool"
 
-module.exports.connectToMongo = () => {
+const connectToMongo=()=>{
+    mongoose.connect(mongoURI, ()=>{
+        console.log("mongo connection successfully created");
+    })
+}
+
+module.exports = connectToMongo;
+
+
+// module.exports.connectToMongo =async  () => {
+//     return new Promise((resolve, reject) => {
+//         mongoose
+//             .connect(mongoURI, {
+//                 useNewUrlParser: true,
+//                 useUnifiedTopology: true
+//             },(err,result)=>{
+//                 if(err){
+//                     let response={
+//                         err:err
+//                     }
+//                     console.log("error");
+//                     reject(response);
+//                 }else{
+//                     console.log("success");
+//                     resolve("connected success");
+//                 }
+//             })
+//     })
+// }
+
+
+// module.exports.connectToMongo = async () => {
+    
+//         .then(() => console.log("Database connected!"))
+//         .catch((err) => console.log(err));
+// }
+
+module.exports.connectToMongo = async () => {
     mongoose
         .connect(mongoURI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
         .then(() => console.log("Database connected!"))
-        .catch(err => console.log(err));
+        .catch((err) => console.log(err));
 }
 
